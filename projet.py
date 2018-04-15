d212987bd8c72c4b66c19059a46f04a59a8c328f
Titanreotra <mohamedtra96@hotmail.com>
2018-04-15 18:01:39 +0000

    redirections sans enchainement fonctionne

--- projet.py
+++ projet.py
@@ -4,25 +4,39 @@ import lexer as ssp
 #  **** fonction pour excuter*****
 
 def executerCommandeSimple(processus):
-
-	(rfd,wfd)=os.pipe() # derscrtiteur de lecture et dectiture de pipe
 	pid=os.fork() # 
 	if(pid==0): # le fils s'ocuupe de l'excution de la commande
-		os.close(rfd)# fermeture de cote lecture du pipe
 		commande=processus._cmd.getCommand()
 		argCommande=processus._cmd.getArgs()
-		# print("je vais executer " + commande)
-		# print("je suis le fils %d" %(os.getpid()))
-
 		redirEntree=filtrerRedirectionsEntree(processus)
-		#print(redirEntree)
+		
 		if redirEntree:
 			#print(redirEntree._filespec)
 			re_re_fd=os.open(redirEntree._filespec, os.O_RDONLY)
-			#print(re_fd)
 			os.dup2(re_re_fd, 0)
 			os.close(re_re_fd)
 
+		re_wr_fd=None
+		redirSortie=filtrerRedirectionsSortie(processus) # redirection de la sortie gerer par le pere
+		# print('r')
+		# print(redirSortie)
+		if redirSortie:
+			# os.write(2, b'red sortie')
+			if redirSortie.isAppend():
+				re_wr_fd=os.open(redirSortie._filespec, os.O_WRONLY | os.O_APPEND | os.O_CREAT | os.O_TRUNC)
+			else:
+				re_wr_fd=os.open(redirSortie._filespec, os.O_WRONLY| os.O_CREAT | os.O_TRUNC)
+
+
+			os.dup2(re_wr_fd,1)
+
+			# os.close(re_wr_fd)
+		else: 
+			pass
+			# os.write(2,b'Pas de redirSortie')
+
+
+		
 
 		redirErreur=filtrerRedirectionsErreur(processus)# redirection d'erreur gerer par le fils 
 		re_Er_fd=None
@@ -31,10 +45,10 @@ def executerCommandeSimple(processus):
 				re_Er_fd=os.open(redirErreur._filespec, os.O_WRONLY | os.O_APPEND | os.O_CREAT)
 			else:
 				re_Er_fd=os.open(redirErreur._filespec, os.O_WRONLY| os.O_CREAT)
-			os.write(2, b'rediriger erreur')
+			# os.write(2, b'rediriger erreur')
 			os.dup2(re_Er_fd,2)	
 
-		os.dup2(wfd,1)# utiliser wfd au lieu de la sortie standard
+		#os.dup2(wfd,1)# utiliser wfd au lieu de la sortie standard
 		argCommande=[commande]+ argCommande # pour tt les fonction exec le 1er argCommande doit etre la commande lui meme
 
 		try: # essaye cette commande 
@@ -50,49 +64,16 @@ def executerCommandeSimple(processus):
 							os.execv("./"+commande, argCommande)
 						except OSError as e:
 						
-							os.write(2,byteaargCommanderray(e.strerror,"utf-8")) # affiche dans la sortie d'erreur standard
+							os.write(2,bytearray(argCommanderray(e.strerror,"utf-8"))) # affiche dans la sortie d'erreur standard
 				
-			print("coucou")
 			
 	else: # pere 
 
-		os.close(wfd) # fermeture du cote  ecriture du pipe
 		# print("j attends le %d\n" %(pid))
-		#time.sleep(1)
 		(p,es) = os.waitpid(pid,0)
-		#print("j'ai fini attendre pour %d qui a %s\n" %(pid,os.WIFEXITED(es)))
+		# print("j'ai fini attendre pour %d qui a %s\n" %(pid,os.WIFEXITED(es)))
 
 
-		re_wr_fd=None
-		redirSortie=filtrerRedirectionsSortie(processus) # redirection de la sortie gerer par le pere
-		print('r')
-		print(redirSortie)
-		if redirSortie:
-			# os.write(2, b'red sortie')
-			if redirSortie.isAppend():
-				re_wr_fd=os.open(redirSortie._filespec, os.O_WRONLY | os.O_APPEND | os.O_CREAT | os.O_TRUNC)
-			else:
-				re_wr_fd=os.open(redirSortie._filespec, os.O_WRONLY| os.O_CREAT | os.O_TRUNC)
-
-			os.dup2(re_wr_fd,1, inheritable=False)
-
-			# os.close(re_wr_fd)
-		else: 
-			pass
-			# os.write(2,b'Pas de redirSortie')
-
-
-		while True:
-			morceau=os.read(rfd,3)
-			if not morceau:
-				break
-			os.write(1, morceau)# ecrit dans la sortie standard 
-
-		os.close(rfd) # fermeture du cote lecture du pipe
-		if re_wr_fd:
-			os.dup2(1, re_wr_fd)
-			os.close(re_wr_fd)
-			# os.write(2,b'fermer re_wr_fd')
 
 def log(msg): # pour voir les erreur sur le code
 	log_fd=os.open("log.txt", os.O_CREAT | os.O_WRONLY )
@@ -112,7 +93,7 @@ def filtrerRedirectionsErreur(processus):
 
 if __name__ =='__main__':
 	nomPipe="/tmp/pomme"
-	pl=ssp.get_parser().parse("sh imp.sh > sortie.txt | sh imp.sh > cerise.txt | sh imp.sh")
+	pl=ssp.get_parser().parse("sh imp.sh > sortie.txt | sh imp.sh 2> cerise.txt | wc -c < cerise.txt  ")
 	# pl = ssp.get_parser().parse("sh ficTest2.sh > ghi.txt 2> cerise.txt")
 	for p in pl:
 		# print(p)
